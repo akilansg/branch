8313b6b3c5b5884e5156bf6d19b8abdf66643ce3
akilansg <akilanguna2006@gmail.com>
2025-01-05 18:28:27 +0530

    af

--- add.php
+++ add.php
@@ -5,8 +5,8 @@ function add($num1, $num2) {
 }
 
 // Example usage
-$number1 = 100000;
-$number2 = 15000;
+$number1 = 15689;
+$number2 = 15444440;
 $sum = add($number1, $number2);
 
 echo "The sum of $number1 and $number2 is: $sum";
