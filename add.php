1d099cd6b275ebe4756cf7b25d197108e4b01db1
akilansg <akilanguna2006@gmail.com>
2025-01-05 18:21:10 +0530

    aae

---
+++ add.php
@@ -0,0 +1,13 @@
+<?php
+// Function to add two numbers
+function add($num1, $num2) {
+    return $num1 + $num2;
+}
+
+// Example usage
+$number1 = 10;
+$number2 = 15;
+$sum = add($number1, $number2);
+
+echo "The sum of $number1 and $number2 is: $sum";
+?>
